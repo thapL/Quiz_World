89ea8c9328eeb033e8fbbf59780f2d3ec7f130ce
Jay <suraipaza@gmail.com>
2025-06-11 06:11:04 +0700

    adap flashCard and add word

--- my-app/src/components/Flashcard.jsx
+++ my-app/src/components/Flashcard.jsx
@@ -1,17 +1,36 @@
-export default function Flashcard({ flashIndex, setFlashIndex, setMode, words }) {
+export default function Flashcard({ flashIndex, setFlashIndex, setMode, words}) {
   return (
     <>
-      <p className="text-2xl font-semibold text-[#ffaa00]">{words[flashIndex].word}</p>
-      <p className="text-xl mt-2 text-[#fff8f0]">{words[flashIndex].meaning}</p>
+    <p className="text-2xl font-semibold text-[#ffaa00] text-center">
+      {words[flashIndex].word}
+    </p>
+
+    <div className="flex justify-center my-4">
+      <div className="w-60 h-50 border-4 border-[#ffaa00] rounded-xl overflow-hidden shadow-lg">
+        <img
+          src={words[flashIndex].img}
+          className="w-full h-full object-cover"
+          alt=""
+        />
+      </div>
+    </div>
+
+    <p className="text-xl mt-2 text-[#fff8f0] text-center">
+      {words[flashIndex].meaning}
+    </p>
+
+    <div className="flex justify-center">
       <button
         onClick={() => {
           if (flashIndex + 1 === words.length) setMode("quiz");
           else setFlashIndex(flashIndex + 1);
         }}
-        className="mt-6  bg-[#00a58b] hover:bg-[#00dec0] text-[#fff8f0] font-semibold py-2 px-4 rounded-full transition-all shadow-lg hover:scale-105"
+        className="mt-6 bg-[#00a58b] hover:bg-[#00dec0] text-[#fff8f0] font-semibold py-2 px-6 rounded-full transition-all shadow-lg hover:scale-105"
       >
         {flashIndex + 1 === words.length ? "เริ่มแบบทดสอบ" : "ถัดไป"}
       </button>
-    </>
+    </div>
+</>
+
   );
 }
